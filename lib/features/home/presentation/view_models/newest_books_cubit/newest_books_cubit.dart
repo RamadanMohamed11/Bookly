815a2123d9fd0048b2efc4884070118d9b2a5fcf
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-09-12 04:42:04 +0300

    create NewestBooksCubit

---
+++ lib/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    HomeRepo homeRepo = HomeRepoImpl();
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.errorMessage)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
