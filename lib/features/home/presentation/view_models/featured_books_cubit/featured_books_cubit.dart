dd1d0013c9cd111b2844b40aace1e73e7ba9c9fa
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-09-12 03:56:29 +0300

    create fetchFeaturedBooks method

--- lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit() : super(FeaturedBooksInitial());
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    HomeRepo homeRepo = HomeRepoImpl();
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
 }
